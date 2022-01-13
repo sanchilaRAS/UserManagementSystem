92fc9243d1eb9597506d8435a79f49366a3b75e4
sanchilaRAS <sanchilaranaweera@gmail.com>
2022-01-13 08:09:02 +0530

    User management project- Bootstrap

---
+++ src/main/java/com/mycompany/user/UserService.java
@@ -0,0 +1,40 @@
+package com.mycompany.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired private UserRepository repo;
+
+    public List<User> listAll(){
+        return (List<User>) repo.findAll();
+    }
+    public void save(User user) {
+        //System.out.println(user.getEmail());
+        //System.out.println(user.getFirstname());
+        repo.save(user);
+    }
+
+    public User get(Integer id) throws UserNotFoundException {
+        Optional<User> result = repo.findById(id);
+        if(result.isPresent()){
+            return result.get();
+        }
+        throw new UserNotFoundException("Could not found any user with ID"+ id);
+
+    }
+
+    public void delete(Integer id) throws UserNotFoundException {
+        Long count=repo.countById(id);
+        if(count==null || count ==0){
+            throw new UserNotFoundException("Could not found any user with ID"+ id);
+        }
+        repo.deleteById(id);
+    }
+
+}
